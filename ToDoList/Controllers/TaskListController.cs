dfbd81f9e5295b1ea3c63e9a48e3fe895ec05168
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-11-24 15:25:38 +0600

    Add project files.

---
+++ ToDoList/Controllers/TaskListController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers;
+
+[Route("task-handler")]
+[ApiController]
+public class TaskListController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public TaskListController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetTask()
+    {
+        IEnumerable<TaskList> taskLists = _context.Tasks.ToList();
+        return Ok(taskLists);
+    }
+
+    [HttpPost]
+    public IActionResult CreateTask(TaskList obj)
+    {
+        _context.Tasks.Add(obj);
+        _context.SaveChanges();
+        return Ok();
+    }
+}
---
+++ ToDoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TaskList> Tasks { get; set; }
+}
